21cf29167346a9b51c4a67de668803c68a087848
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-08-28 14:09:39 +0530

    First commit / units of Numbers

---
+++ src/Units.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Units {
+    public static void main(String[] args) {
+        System.out.print("Enter a numer from (1,10,100,1000) : ");
+        Scanner x = new Scanner(System.in);
+        int Num = x.nextInt();
+
+        if (Num == 1) {
+            System.out.print("unit");
+        } else if (Num == 10) {
+            System.out.print("TEN");
+        } else if (Num == 100) {
+            System.out.print("HUNDRED");
+        } else if (Num == 1000) {
+            System.out.print("THOUSAND");
+        } else {
+            System.out.print("Enter a Number from (1,10,100,1000)");
+        }
+    }
+}
